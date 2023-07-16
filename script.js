196e2ae0d177ba02ce28646eb3dd0eb73989ffea
Imforpeace <mr.imforpeace.xx@gmail.com>
2023-07-16 04:06:15 -0600

    Parametricity support and security fix
    Changed the logic to clean up the html file.
    Switched from eval() functions to JSON for letter-mapping.
    The script also directly reads the innertext of animatedtext id div in realtime. if the innertext is manually edited — the script will automatically regenerate the pattern for the new innerText.

--- script.js
+++ script.js
@@ -1,106 +1,132 @@
-//Written by Gurgen Efendiyev, ifp.pw
+// Written by Gurgen Gurgenyan, circion.design
+// A lot of the script was written a while ago and I slapped parametricity just a second ago.
 
-try {
-    var text = "The quick brown fox jumps over the lazy dog";
-	
-	
-	
-    //There's probably a smarter way to do this, but it's gonna work so who cares
+var refreshinterval = 100;
+
+//TODO parametric amount of swapped letters per pattern
+//var letterswapcount = 2;
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    var text = document.getElementById("animatedtext").innerText;
+    var changearray = [];
     var textarray = [];
     var textarray_help = [];
-    var A = ["A", "a", "4"],
-        B = ["B", "b"],
-        C = ["C", "c"],
-        D = ["D", "d"]
-        E = ["E", "e", "%", "3"],
-        F = ["F", "f", "7"],
-        G = ["G", "g"],
-        H = ["H", "H"],
-        I = ["I", "1", "i", "L", "!"],
-        J = ["J", "j"],
-        K = ["K", "k"],
-        L = ["L", "l"],
-        M = ["M", "m", "#"],
-        N = ["N", "n"],
-        O = ["O", "o", "0"],
-        P = ["P", "p"],
-        Q = ["q", "q"],
-        R = ["R", "r"],
-        S = ["S", "s"],
-        T = ["T", "t"],
-        U = ["U", "u"],
-        V = ["V", "v"],
-        W = ["W", "w"],
-        X = ["X", "x"],
-        Y = ["Y", "y"],
-        Z = ["Z", "z"];
+
+    //Usage: lettermap["A"][2]
+    var lettermap =
+    {
+        "A": ["A", "a", "4"],
+        "B": ["B", "b"],
+        "C": ["C", "c"],
+        "D": ["D", "d"],
+        "E": ["E", "e", "3"],
+        "F": ["F", "f", "7"],
+        "G": ["G", "g"],
+        "H": ["H", "H"],
+        "I": ["I", "1", "i", "L", "!"],
+        "J": ["J", "j"],
+        "K": ["K", "k"],
+        "L": ["L", "l"],
+        "M": ["M", "m", "#"],
+        "N": ["N", "n", "#"],
+        "O": ["O", "o", "0"],
+        "P": ["P", "p"],
+        "Q": ["q", "q"],
+        "R": ["R", "r"],
+        "S": ["S", "s", "$"],
+        "T": ["T", "t"],
+        "U": ["U", "u", "%"],
+        "V": ["V", "v"],
+        "W": ["W", "w"],
+        "X": ["X", "x"],
+        "Y": ["Y", "y"],
+        "Z": ["Z", "z"]
+    }
 
 
 
-    	const lines = text.split('');
+    function isLetter(str) {
+        return str.length === 1 && str.match(/[a-z]/i);
+    }
 
-	function isLetter(str) {
-  		return str.length === 1 && str.match(/[a-z]/i);
-	}
-    
     function shuffle(array) {
-        let currentIndex = array.length,  randomIndex;
-      
+        let currentIndex = array.length, randomIndex;
+
         // While there remain elements to shuffle...
-            while (currentIndex != 0) {
-            
-              // Pick a remaining element...
-              randomIndex = Math.floor(Math.random() * currentIndex);
-              currentIndex--;
-            
-              // And swap it with the current element.
-              [array[currentIndex], array[randomIndex]] = [
+        while (currentIndex != 0) {
+
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+
+            // And swap it with the current element.
+            [array[currentIndex], array[randomIndex]] = [
                 array[randomIndex], array[currentIndex]];
-            }
+        }
         return array;
     }
-      
-    for (var i = 0; i < text.length; i++)
-    {
-        textarray[i] = i;
-    }
-
-    var changearray = [];
-    for(var changecount = 0; changecount < 100; changecount++)
-    {
-        shuffle(textarray);
-        for(var i = 0; i < lines.length; i++)
-        {
-            textarray_help[i] = lines[textarray[i]].toUpperCase();
-        }
-        if(isLetter(textarray_help[0]) && isLetter(textarray_help[1]))
-	    {
-        	var zero = eval(textarray_help[0]);
-        	var two = eval(textarray_help[1]);
+    function generatePattern() {
+        let lines = text.split('');
+        for (var i = 0; i < text.length; i++) {
+            textarray[i] = i;
         }
 
-        var changethis = text;
-        var which = Math.random();
-        if(which==0)
-        {
-            const changethis_arrayed = changethis.split('');
-	    if(isLetter(textarray_help[0])	 && isLetter(textarray_help[1]))
-    	    {
-            	changethis_arrayed[textarray[0]] = eval(lines[textarray[0]].toUpperCase())[Math.floor(Math.random() * eval(zero).length)];
-	    }
+
+        for (var changecount = 0; changecount < 100; changecount++) {
+            shuffle(textarray);
+            for (var i = 0; i < lines.length; i++) {
+                textarray_help[i] = lines[textarray[i]].toUpperCase();
+            }
+
+            if (isLetter(textarray_help[0]) && isLetter(textarray_help[1])) {
+                var zero = lettermap[textarray_help[0]];
+                var two = lettermap[textarray_help[1]];
+            }
+
+            var changethis = text;
+            var which = Math.random();
+            if (which == 0) {
+                const changethis_arrayed = changethis.split('');
+                if (isLetter(textarray_help[0]) && isLetter(textarray_help[1])) {
+                    changethis_arrayed[textarray[0]] = lettermap[lines[textarray[0]].toUpperCase()][Math.floor(Math.random() * zero.length)];
+                }
+            }
+            else {
+                const changethis_arrayed = changethis.split('');
+                if (isLetter(textarray_help[0]) && isLetter(textarray_help[1])) {
+                    changethis_arrayed[textarray[0]] = lettermap[lines[textarray[0]].toUpperCase()][Math.floor(Math.random() * zero.length)];
+                    changethis_arrayed[textarray[1]] = lettermap[lines[textarray[1]].toUpperCase()][Math.floor(Math.random() * two.length)];
+                }
+                var final = changethis_arrayed.join('');
+            }
+            changearray[changecount] = final;
         }
-        else
-        {
-            const changethis_arrayed = changethis.split('');
-            if(isLetter(textarray_help[0])	 && isLetter(textarray_help[1]))
-    	    {
-                changethis_arrayed[textarray[0]] = eval(lines[textarray[0]].toUpperCase())[Math.floor(Math.random() * eval(zero).length)];
-                changethis_arrayed[textarray[1]] = eval(lines[textarray[1]].toUpperCase())[Math.floor(Math.random() * eval(two).length)];
-	    }
-            var final = changethis_arrayed.join('');
-        }
-        changearray[changecount] = final;
+        document.getElementById("animatedtext").textContent = text;
     }
-} catch (err) {
-    console.error(err);
-}
+
+    function durationSlider() {
+        var count = 0;
+        var prevselector;
+        setInterval(function () {
+            var arrayselector = Math.floor(Math.random() * 99);
+            var changedtext = document.getElementById("animatedtext");
+
+            if (changearray[prevselector] != changedtext.innerText) {
+                text = document.getElementById("animatedtext").innerText;
+                generatePattern();
+            }
+            changedtext.textContent = changearray[arrayselector];
+            prevselector = arrayselector;
+
+            count += 1;
+            if (count >= 99) {
+                count = 0;
+            }
+        }, refreshinterval);
+
+    }
+
+    generatePattern();
+    durationSlider();
+});
